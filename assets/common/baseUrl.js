40d75eb21017a2a94be3bede73e5aa8d5ec5020d
Minazuna <159107536+Minazuna@users.noreply.github.com>
2025-03-29 19:43:27 +0800

    product form

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -4,7 +4,7 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.0.103:4000'
+? baseURL = 'http://192.168.100.115:4000'
 : baseURL = 'http://192.168.0.103:4000'
 }
 
